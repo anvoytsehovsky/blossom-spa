bcf7737aa90ff5ff09faf9c00bc5cec40ff2bda9
Andrew Voytsehovsky <anvoytsehovsky@gmail.com>
2022-04-30 15:37:31 +0300

    video component

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Video = () => {
   return (
-	<div>Video</div>
+	<Section>
+    <div className="background"></div>
+    <div className="video">
+    <iframe
+      width="960"
+      height="515"
+      src="https://www.youtube.com/embed/2ffOXOfZzn0"
+      title="YouTube video player"
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+    ></iframe>
+    </div>
+  </Section>
   )
 }
+
+const Section = styled.section`
+  background-color: #662d91aa;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  .video {
+    z-index: 10;
+  }
+`
 
 export default Video
