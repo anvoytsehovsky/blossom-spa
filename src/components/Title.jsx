cc1e259133822f17fecb266d2cb8c7441c98e47f
Andrew Voytsehovsky <anvoytsehovsky@gmail.com>
2022-04-30 08:37:23 +0300

    added Services & Title

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import styled from 'styled-components'
 
-const Title = () => {
+const Title = ({ value }) => {
   return (
-	<div>Title</div>
+	<Div>
+    <h1>{value}</h1>
+  </Div>
   )
 }
+
+const Div = styled.div`
+  position: relative;
+  h1 {
+    position: absolute;
+    transform: translateX(-30%) translateY(20vw) rotate(-90deg);
+    font-size: 7rem;
+    text-transform: uppercase;
+    color: var(--secondary-color);
+    font-weight: 400;
+  }
+`
 
 export default Title
