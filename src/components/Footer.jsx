f72d668df51514fe6a2adfd43323c862c23959c8
Andrew Voytsehovsky <anvoytsehovsky@gmail.com>
2022-05-02 14:01:00 +0300

    Footer Component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import styled from 'styled-components'
+import { BsFacebook, BsTwitter, BsYoutube, BsBehance } from 'react-icons/bs';
 
 const Footer = () => {
   return (
-	<div>Footer</div>
+	<Foot>
+    <span>
+      &copy; React Website Transition Animation
+    </span>
+    <div className="footer__social__icon">
+      <BsFacebook />
+      <BsTwitter />
+      <BsYoutube />
+      <BsBehance />
+    </div>
+  </Foot>
   )
 }
+
+const Foot = styled.footer`
+  background-color: var(--primary-color);
+  color: #fff;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 5rem 10rem;
+  .footer__social__icon {
+    display: flex;
+    gap: 2rem;
+    svg {
+      font-size: 1.4rem;
+      cursor: pointer;
+      transition: 0.5s ease-in-out;
+      &:hover {
+        color: var(--secondary-color);
+      }
+    }
+  }
+`
 
 export default Footer
