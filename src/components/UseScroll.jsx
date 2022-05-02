993438b0da412784a7f8bef8f621d2188e2a658f
Andrew Voytsehovsky <anvoytsehovsky@gmail.com>
2022-05-02 14:29:21 +0300

    ScrollToTop & useScroll

--- src/components/UseScroll.jsx
+++ src/components/UseScroll.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { useInView } from "react-intersection-observer"
+import { useAnimation } from "framer-motion"
 
-const UseScroll = () => {
-  return (
-	<div>UseScroll</div>
-  )
+const UseScroll = (thresh = 0.1) => {
+  const controls = useAnimation()
+  const [element, view] = useInView({ threshold: thresh })
+
+  if (view) {
+    controls.start("show")
+  } else {
+    controls.start("hidden")
+  }
+  return [element, controls]
 }
 
 export default UseScroll
